a520ee86a884604b2183516e6e896417baf880d8
James Dave Navor <esteban.dave999@gmail.com>
2023-03-30 19:07:13 +0800

    Added uID property

--- app/src/main/java/com/example/kodegojobsearchapp/model/User.kt
+++ app/src/main/java/com/example/kodegojobsearchapp/model/User.kt
@@ -2,6 +2,7 @@ package com.example.kodegojobsearchapp.model
 
 open class User() {
     var id: Int = 0
+    var uID: String = "" /** Firebase User ID */
     var lastname: String = ""
     var firstname: String = ""
     var middlename: String = ""
