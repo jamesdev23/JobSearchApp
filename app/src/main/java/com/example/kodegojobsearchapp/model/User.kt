eec084f23b06aa33314b8926582534b7fd067985
James Dave Navor <esteban.dave999@gmail.com>
2023-03-30 19:07:52 +0800

    Optimized naming scheme

--- app/src/main/java/com/example/kodegojobsearchapp/model/User.kt
+++ app/src/main/java/com/example/kodegojobsearchapp/model/User.kt
@@ -3,9 +3,9 @@ package com.example.kodegojobsearchapp.model
 open class User() {
     var id: Int = 0
     var uID: String = "" /** Firebase User ID */
-    var lastname: String = ""
-    var firstname: String = ""
-    var middlename: String = ""
+    var lastName: String = ""
+    var firstName: String = ""
+    var middleName: String = ""
     var email: String = ""
     var type: String = ""
     var image: String = ""
