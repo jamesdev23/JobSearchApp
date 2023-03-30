7406b53403669f4ffbcafe1b72e7c3b29041b48d
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-03-30 17:02:17 +0800

    Added initial packages Updated app gradle Added SignInActivity.kt

---
+++ app/src/main/java/com/example/kodegojobsearchapp/SignInActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kodegojobsearchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        // note: for firebase login. touch this when implementing firebase login
+        // - nico 3/30/23
+    }
+}
