c9b8a05787372278bc405dff32d595649f99c75b
James Dave Navor <esteban.dave999@gmail.com>
2023-04-11 17:57:21 +0800

    Extended ApplicantDAO

--- app/src/main/java/com/example/kodegojobsearchapp/firebase/FirebaseApplicantDAO.kt
+++ app/src/main/java/com/example/kodegojobsearchapp/firebase/FirebaseApplicantDAO.kt
@@ -10,18 +10,18 @@ interface FirebaseApplicantDAO {
     suspend fun addApplicant(applicant: Applicant): Boolean
     suspend fun getApplicant(uID: String): Applicant
     suspend fun getApplicants(): ArrayList<Applicant>
-    suspend fun updateApplicant(fields: HashMap<String, Any?>): Boolean //TODO: Not yet implemented
-    suspend fun deleteApplicant()
+    suspend fun updateApplicant(applicant: Applicant, fields: HashMap<String, Any?>): Boolean //TODO: Not yet implemented
+    suspend fun deleteApplicant(applicant: Applicant): Boolean
 }
 
 class FirebaseApplicantDAOImpl(context: Context): FirebaseUserDAOImpl(context), FirebaseApplicantDAO{
     private val collection = FirebaseCollections.Applicants
+    private val reference = fireStore.collection(collection)
+
     override suspend fun addApplicant(applicant: Applicant): Boolean {
-        val reference = fireStore
-            .collection(collection)
-            .document()
-        applicant.applicantID = reference.id
-        val task = reference.set(applicant.exportFirebaseApplicant(), SetOptions.merge())
+        val document = reference.document()
+        applicant.applicantID = document.id
+        val task = document.set(applicant.exportFirebaseApplicant(), SetOptions.merge())
         task.await()
         return if (task.isSuccessful){
             Log.i("Applicant Creation", "Successful")
@@ -33,19 +33,58 @@ class FirebaseApplicantDAOImpl(context: Context): FirebaseUserDAOImpl(context), 
     }
 
     override suspend fun getApplicant(uID: String): Applicant {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        val task = reference
+            .document(uID)
+            .get()
+        task.await()
+        return if (task.isSuccessful && task.result.data != null){
+            Log.i("Applicant", task.result.toString())
+            task.result.toObject(Applicant::class.java)!!
+        }else{
+            Log.e("GetApplicant", task.exception?.message.toString())
+            val applicant = Applicant()
+            applicant.uID = uID
+            addApplicant(applicant)
+            applicant
+        }
     }
 
     override suspend fun getApplicants(): ArrayList<Applicant> {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        val applicants: ArrayList<Applicant> = ArrayList()
+        val task = reference.get()
+        task.await()
+        if (task.isSuccessful && task.result.documents.isNotEmpty()){
+            Log.i("Applicants", task.result.documents.toString())
+            task.result.documents.forEach { snapshot ->
+                Log.i("Applicant Data", snapshot.data.toString())
+                val applicant = snapshot.toObject(Applicant::class.java)!!
+                applicant.setUser(getUser(applicant.uID)!!)
+                Log.d("Applicant", applicant.toString())
+                applicants.add(applicant)
+            }
+        }else{
+            Log.e("GetApplicants", task.exception?.message.toString())
+        }
+        return applicants
     }
 
-    override suspend fun updateApplicant(fields: HashMap<String, Any?>): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun updateApplicant(applicant: Applicant, fields: HashMap<String, Any?>): Boolean {
+//        TODO("Not yet implemented")
+        val task = reference.document(applicant.applicantID)
+            .update(fields)
+        task.await()
+        return task.isSuccessful
     }
 
-    override suspend fun deleteApplicant() {
-        TODO("Not yet implemented")
+    override suspend fun deleteApplicant(applicant: Applicant): Boolean {
+//        TODO("Not yet implemented")
+        val task = reference
+            .document(applicant.applicantID)
+            .delete()
+        task.await()
+        return task.isSuccessful
     }
 
 }
