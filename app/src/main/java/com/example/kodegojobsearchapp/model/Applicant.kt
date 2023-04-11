792930dfabba572fe06b759fe813c3049d9fb254
James Dave Navor <esteban.dave999@gmail.com>
2023-04-11 18:30:49 +0800

    Setup IndeedAPI Model

--- app/src/main/java/com/example/kodegojobsearchapp/model/Applicant.kt
+++ app/src/main/java/com/example/kodegojobsearchapp/model/Applicant.kt
@@ -7,6 +7,9 @@ class Applicant(): User() {
     var occupation: String = ""
     var description: String = ""
 
+    /**
+     * Separate unnecessary inherited data from Super Class to prevent redundant data storage
+     */
     fun exportFirebaseApplicant(): FirebaseApplicant{
         return FirebaseApplicant(uID, applicantID, occupation, description)
     }
