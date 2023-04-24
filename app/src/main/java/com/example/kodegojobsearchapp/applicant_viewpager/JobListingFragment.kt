0aa05d5d40ebdb06eebfd398e81b3a0c1e3e51fa
James Dave Navor <esteban.dave999@gmail.com>
2023-04-24 10:11:06 +0800

    Implemented DAO Might be faulty. Revert from TODO Comments in case of errors

--- app/src/main/java/com/example/kodegojobsearchapp/applicant_viewpager/JobListingFragment.kt
+++ app/src/main/java/com/example/kodegojobsearchapp/applicant_viewpager/JobListingFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.lifecycleScope
 import com.example.kodegojobsearchapp.adapter.JobListingDataAdapter
 import com.example.kodegojobsearchapp.api.JobSearchAPIClient
 import com.example.kodegojobsearchapp.api_model.JobListingData
@@ -15,11 +17,10 @@ import com.example.kodegojobsearchapp.api_model.JobSearchResultResponse
 import com.example.kodegojobsearchapp.dao.JobListingDAO
 import com.example.kodegojobsearchapp.dao.KodegoJobSearchApplication
 import com.example.kodegojobsearchapp.databinding.FragmentJobListingBinding
-import com.example.kodegojobsearchapp.utils.ProgressDialog
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.http.HTTP
 import java.net.HttpURLConnection
 
 class JobListingFragment : Fragment() {
@@ -62,7 +63,8 @@ class JobListingFragment : Fragment() {
         binding.appJobListing.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.appJobListing.adapter = jobListingDataAdapter
 
-        getData(defaultQuery, defaultPage)
+//        getData(defaultQuery, defaultPage)
+        getDataFromDAO()
 
         binding.searchJobListing.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
@@ -81,17 +83,34 @@ class JobListingFragment : Fragment() {
         })
 
         binding.btnPrevious.setOnClickListener {
-            currentPage -= 1
-            if (currentPage >= 1) {
-                getData(currentQuery, currentPage)
+            if (currentPage > 1) {
+                currentPage -= 1
+//                getData(currentQuery, currentPage)
+                getDataFromDAO() //TODO: Revert in case of errors
             }
         }
 
         binding.btnNext.setOnClickListener {
             currentPage += 1
-            getData(currentQuery, currentPage)
+//            getData(currentQuery, currentPage)
+            getDataFromDAO() //TODO: Revert in case of errors
         }
 
+    }
+
+    private fun getDataFromDAO(){
+        binding.appJobListing.visibility = View.GONE
+        binding.loadingData.visibility = View.VISIBLE
+        lifecycleScope.launch {
+            val list = dao.getJobLists((currentPage - 1) * 10) as ArrayList<JobListingData>
+            if (list.isEmpty()){
+                getData(currentQuery, currentPage)
+            }else{
+                jobListingDataAdapter.setList(list)
+                binding.appJobListing.visibility = View.VISIBLE
+                binding.loadingData.visibility = View.GONE
+            }
+        }
     }
 
     private fun getData(queryText: String, page: Int){
@@ -104,6 +123,7 @@ class JobListingFragment : Fragment() {
             override fun onFailure(call: Call<JobSearchResultResponse>, t: Throwable) {
                 binding.appJobListing.visibility = View.VISIBLE
                 binding.loadingData.visibility = View.GONE
+                currentPage--
 
                 Log.d("API CALL", "Failed API CALL")
                 Log.e("error", t.message.toString())
@@ -117,15 +137,26 @@ class JobListingFragment : Fragment() {
                 binding.loadingData.visibility = View.GONE
                 if (response.isSuccessful) {
                     val response: JobSearchResultResponse = response.body()!!
-                    response.status
 
-                    jobListingDataAdapter.setList(response.dataList)
-
-                    val dataLists = response.dataList
-                    for (data in dataLists) {
-                        Log.d("API CALL", "${data.jobTitle} ${data.employerName}")
+                    val dataList = response.dataList
+                    if(dataList.isNotEmpty()) {
+                        jobListingDataAdapter.setList(dataList)
+                        lifecycleScope.launch {
+                            for (data in dataList) {
+                                dao.insert(data)
+                                Log.d("API CALL", "${data.jobTitle} ${data.employerName}")
+                            }
+                        }
+                    }else{
+                        currentPage--
+                        Toast.makeText(
+                            requireContext(),
+                            "You've reached the end of results",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }else{
+                    currentPage--
                     when (response.code()){
                         HttpURLConnection.HTTP_BAD_REQUEST -> Log.e("API Call", "Bad Request")
                         HttpURLConnection.HTTP_NOT_FOUND -> Log.e("API Call", "Not Found")
