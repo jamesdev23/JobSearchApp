b6124b448512b88539480fae31c9abda078a8b62
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-04-28 04:07:19 +0800

    added splash screen

---
+++ app/src/main/java/com/example/kodegojobsearchapp/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.example.kodegojobsearchapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.kodegojobsearchapp.applicant_viewpager.ApplicantViewPagerActivity
+import com.example.kodegojobsearchapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private val splashDisplayLength = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this, ApplicantViewPagerActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, splashDisplayLength.toLong())
+    }
+}
